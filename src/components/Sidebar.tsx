e2dad7b41cd6b0dff4cd11394c6ade2a056be706
rafaelmpardim <rafaelmedeirospardim@gmail.com>
2022-10-31 19:48:49 -0300

    feat: creating Sidebar component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,32 @@
+import { PencilLine } from 'phosphor-react'
+
+import { Avatar } from './Avatar'
+
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1521185496955-15097b20c5fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <Avatar
+          hasBorder
+          src="https://github.com/rafaelmpardim.png"
+        />
+        <strong>Rafael Pardim</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
